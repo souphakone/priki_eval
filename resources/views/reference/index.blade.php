3d7c18385f02ba824eb378e4cc93f123ea500c6b
SAMOUTPHONH Souphakone <Souphakone.SAMOUTPHONH@cpnv.ch>
2022-02-19 10:11:34 +0100

    change view of reference

--- resources/views/reference/index.blade.php
+++ resources/views/reference/index.blade.php
@@ -3,15 +3,32 @@
 @section('content')
     <div class="container">
         <div class="text-3xl mb-3">Liste des références</div>
-        @foreach($references as $reference)
-            <div>
-                @if ($reference->url)
-                    <a href="{{ $reference->url }}" target="_blank">{{ $reference->description }}</a>
-                @else
-                    {{ $reference->description }}
-                @endif
-            </div>
-        @endforeach
+        <table class="table-auto border-black rounded">
+            <tr>
+                <th>Auteurs</th>
+                <th>Titres</th>
+                <th>Date de parution</th>
+                <th>Descriptions</th>
+                <th></th>
+            </tr>
+            @foreach($references as $reference)
+            <tr>
+                <td>{{ $reference->author }}</td>
+                <td>{{ $reference->title }}</td>
+                <td>{{ $reference->create_at }}</td>
+                <td>{{ $reference->description }}</td>
+                <td>
+                    <a href="{{ $reference->url }}" target="_blank">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                        Voir
+                    </button>
+                    </a>
+                </td>
+
+            </tr>
+            @endforeach
+        
+        </table>
         <a href="/references/create" class="btn btn-success btn-sm">Ajouter une nouvelle référence</a>
     </div>
 @endsection
